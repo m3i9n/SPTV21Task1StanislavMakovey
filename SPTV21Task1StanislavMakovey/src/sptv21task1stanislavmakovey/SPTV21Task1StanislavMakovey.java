c68ede90d38fd4854713b4cb3125ad8e07f16fc9
StanislavMakovey <113332004+m3i9n@users.noreply.github.com>
2022-09-27 10:51:18 +0300

    Add files via upload

---
+++ SPTV21Task1StanislavMakovey/src/sptv21task1stanislavmakovey/SPTV21Task1StanislavMakovey.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv21task1stanislavmakovey;
+
+/**
+ *
+ * @author fugas
+ */
+public class SPTV21Task1StanislavMakovey {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Java Hello");
+    }
+    
+}
